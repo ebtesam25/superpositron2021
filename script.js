3274641419064012fb2af3987bb8526818e015f9
ebtesam25 <ebtesam@iut-dhaka.edu>
2021-08-15 09:36:56 -0400

    misc

--- script.js
+++ script.js
@@ -1,9 +1,317 @@
-console.log('Loading Shopclusive')
-  
+console.log('Loading Equify')
+var response = {
+    "alternates": [
+        {
+            "delivery": "$3.49 delivery",
+            "extensions": [
+                "Gillette",
+                "Razor",
+                "Cartridge",
+                "Men's",
+                "Five Blade"
+            ],
+            "has_compare_prices": true,
+            "id": "17865073287665593805",
+            "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcT6kScy6HMa69mPfD0R0luyTwDCiay0J8FC7VfKQ0OgGNYa5eqGm0k9RZFt18fKjJtR4T52XVhR&usqp=CAE",
+            "link": "https://www.google.com/shopping/product/17865073287665593805",
+            "merchant": "eBay",
+            "position": 6,
+            "price": 4.99,
+            "price_parsed": {
+                "currency": "USD",
+                "raw": "$4.99",
+                "symbol": "$",
+                "value": 4.99
+            },
+            "price_raw": "$4.99",
+            "rating": 4.5,
+            "reviews": 1169,
+            "snippet": "Gillette razors for men with 5 anti-friction blades. A shave you barely feel Men's razor with Precision Trimmer on back-great ...",
+            "title": "Gillette Fusion5 Razor"
+        },
+        {
+            "delivery": "$4.80 delivery",
+            "has_compare_prices": true,
+            "id": "7107785752164003299",
+            "image": "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQlngQ9yDWj6ekBIpxUdRUaJf7UGI7JbdnGQowrGaNm23blxnsMlVW-fGMKW6sXg7cK8-0byAg&usqp=CAE",
+            "link": "https://www.google.com/shopping/product/7107785752164003299",
+            "merchant": "eBay",
+            "position": 4,
+            "price": 4.99,
+            "price_parsed": {
+                "currency": "USD",
+                "raw": "$4.99",
+                "symbol": "$",
+                "value": 4.99
+            },
+            "price_raw": "$4.99",
+            "rating": 4,
+            "reviews": 954,
+            "snippet": "It provides a close shave at an affordable price. They feature a 3 bladed pivoting head with a Moisture Rich Strip glide and ...",
+            "title": "Gillette Venus Simply 3 Women's Disposable Razor - 4 count"
+        },
+        {
+            "delivery": "Free delivery by Thu, Aug 19",
+            "extensions": [
+                "Gillette",
+                "Men's",
+                "Triple Blade"
+            ],
+            "has_compare_prices": true,
+            "id": "5008628650693593143",
+            "image": "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQWrwlgUAsNwn18SxkM7tWR_cU1qcZZtu1OTwKDlfxGQMt37ISrdnJT5lE1jEpCqjvkqyGiJak&usqp=CAE",
+            "link": "https://www.google.com/shopping/product/5008628650693593143",
+            "merchant": "Walmart",
+            "position": 5,
+            "price": 6.74,
+            "price_parsed": {
+                "currency": "USD",
+                "raw": "$6.74",
+                "symbol": "$",
+                "value": 6.74
+            },
+            "price_raw": "$6.74",
+            "rating": 4.5,
+            "reviews": 628,
+            "snippet": "Our Gillette Mach3 razor blades give you the reliable Gillette shave with comfort and precision. Each razor blade refill is ...",
+            "title": "Gillette Mach3 Men's Razor with 2 Blade Refills"
+        },
+        {
+            "delivery": "$5.99 delivery",
+            "has_compare_prices": true,
+            "id": "1942471716099883913",
+            "image": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSXX9wWprNh2FAyiUQJy6VxaACZqetq1DfLRVP6dp96OdwMR_rYanQU6MYq0vzUKQR0XnPAeeU&usqp=CAE",
+            "link": "https://www.google.com/shopping/product/1942471716099883913",
+            "merchant": "Walmart",
+            "position": 3,
+            "price": 6.97,
+            "price_parsed": {
+                "currency": "USD",
+                "raw": "$6.97",
+                "symbol": "$",
+                "value": 6.97
+            },
+            "price_raw": "$6.97",
+            "rating": 4.5,
+            "reviews": 3898,
+            "snippet": "Discover the secret to an easy, yet indulgently close shave with Venus ComfortGlide White Tea-scented disposable razors ...",
+            "title": "Gillette Venus ComfortGlide White Tea Scented Women's Disposable Razor - 2 count"
+        },
+        {
+            "delivery": "942.0 mi · In stock",
+            "has_compare_prices": true,
+            "id": "8870454657804973221",
+            "image": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSyGRYJD22-Vny4_m_vdK4H1hdzZ81wk5flrNCZ-N0eBazIQz_LMduQ1VmIYjIGFFIEwIioxVs&usqp=CAE",
+            "link": "https://www.google.com/shopping/product/8870454657804973221",
+            "merchant": "Walmart",
+            "position": 16,
+            "price": 6.97,
+            "price_parsed": {
+                "currency": "USD",
+                "raw": "$6.97",
+                "symbol": "$",
+                "value": 6.97
+            },
+            "price_raw": "$6.97",
+            "rating": 4,
+            "reviews": 148,
+            "snippet": "Venus Simply 3 Sensitive Women's Disposable Razor is a 3 bladed razor for sensitive skin. It features SkinElixir lubrication ...",
+            "title": "Gillette Venus Simply 3 Razors, Disposable, Sensitive - 4 razors"
+        }
+    ],
+    "keyword": "gillette razor",
+    "men": {
+        "price": [
+            28.316470588235294,
+            12.97
+        ]
+    },
+    "menmerchants": [
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Gillette",
+            "score": -1
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "eBay",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walgreens",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Gillette",
+            "score": -1
+        },
+        {
+            "name": "Bed Bath & Beyond",
+            "score": 20
+        },
+        {
+            "name": "Target",
+            "score": 100
+        },
+        {
+            "name": "The Art of Shaving",
+            "score": 85
+        },
+        {
+            "name": "pilottop",
+            "score": -1
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Gillette",
+            "score": -1
+        },
+        {
+            "name": "Walgreens",
+            "score": 100
+        },
+        {
+            "name": "Enbanc Health",
+            "score": -1
+        },
+        {
+            "name": "DrugSuppyStore",
+            "score": -1
+        }
+    ],
+    "message": "some random message here",
+    "server": "up",
+    "tax": [
+        -0.8619457251601325,
+        0.1336005344021376
+    ],
+    "women": {
+        "price": [
+            28.316470588235294,
+            12.97
+        ]
+    },
+    "womenmerchants": [
+        {
+            "name": "Walgreens",
+            "score": 100
+        },
+        {
+            "name": "Walmart - Pharmapacks",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "eBay",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walmart - The Penguin Store LLC",
+            "score": 100
+        },
+        {
+            "name": "Walmart - In-novate",
+            "score": 100
+        },
+        {
+            "name": "Target",
+            "score": 100
+        },
+        {
+            "name": "Walgreens",
+            "score": 100
+        },
+        {
+            "name": "eBay - mizzlesworld",
+            "score": 100
+        },
+        {
+            "name": "Walmart - The Online Drugstore",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Sam's Club",
+            "score": -1
+        },
+        {
+            "name": "Walgreens",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "CVS Pharmacy",
+            "score": 90
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walmart",
+            "score": 100
+        },
+        {
+            "name": "Walgreens",
+            "score": 100
+        }
+    ]
+}
+
+
+
+//Amazon
+if(document.getElementById('nav-logo-sprites')){
   const body = document.querySelector("body")
     if(document.getElementById('productTitle')) {
         var button = document.createElement("button");
-            button.innerHTML = "10% Pink Tax";
+            button.innerHTML = "13.36% Pink Tax";
             button.id = "taxButton";
             button.type = "button";
             button.style.backgroundColor = "pink";
@@ -17,13 +325,14 @@ console.log('Loading Shopclusive')
                 console.log(document.getElementById("priceblock_ourprice").textContent);
             });
             var button = document.createElement("button");
-            button.innerHTML = "Shopclusivity Score: 100";
+            button.innerHTML = `CEI Score: ${Math.floor(Math.random() * 100)}`;
             button.id = "scoreButton";
             button.type = "button";
             button.style.backgroundColor = "pink";
             button.style.borderRadius = 20;
             button.style.fontWeight = "bold";
             button.style.border = "none";
+            button.style.marginLeft = "2px";
             button.style.borderWidth = 1;
             button.style.color = "#800000";
             document.getElementById("bylineInfo").appendChild(button);
@@ -35,7 +344,7 @@ console.log('Loading Shopclusive')
 
 
             var div = document.createElement("div");
-            div.innerHTML = "Cheaper Alternatives";
+            div.innerHTML = "Equify Recommends";
             div.id = "altProducts";
             div.type = "div";
             div.style.backgroundColor = "pink";
@@ -52,9 +361,10 @@ console.log('Loading Shopclusive')
                 var br = document.createElement("br");
                 document.getElementById("altProducts").appendChild(br);
                 var product = document.createElement("a");
-                product.innerHTML = `Product ${i} - $ ${i}.00`;
+                product.innerHTML = `${response.alternates[i].title} - $ ${response.alternates[i].price_raw}`;
                 product.id = `suggestedProduct${i}`;
                 product.type = "a";
+                product.href = `${response.alternates[i].link}`
                 product.style.fontWeight = "100";
                 product.style.color = "#800000";
                 product.style.border = "none";
@@ -62,6 +372,113 @@ console.log('Loading Shopclusive')
             }
             
     } else {
-        console.log("Lost")
+        console.log("Lost, Amazon")
     
     }
+}
+
+//Google Shopping
+else if(document.getElementById('gsr')){
+    console.log('Google Shopping')
+    const body = document.querySelector("body")
+    if(document.getElementById('taw')) {
+        var button = document.createElement("button");
+            button.innerHTML = "13.36% Pink Tax";
+            button.id = "taxButton";
+            button.type = "button";
+            button.style.backgroundColor = "pink";
+            button.style.color = "#800000";
+            button.style.borderRadius = "8px";
+            button.style.border = "none";
+            document.getElementById("taw").appendChild(button);
+
+            // var taxBtn = document.getElementById('taw');
+            // taxBtn.addEventListener('click', function() {
+            //     console.log(document.getElementById("taw").textContent);
+            // });
+            var i = 0;
+            for(i=0;i<document.getElementsByClassName("aULzUe IuHnof").length;i++){
+            var button = document.createElement("button");
+            button.innerHTML = `CEI Score: ${Math.floor(Math.random() * 100)}`;
+            button.id = "scoreButton";
+            button.type = "button";
+            button.style.backgroundColor = "pink";
+            button.style.borderRadius = 20;
+            button.style.fontWeight = "bold";
+            button.style.border = "none";
+            button.style.marginLeft = "2px";
+            button.style.borderWidth = 1;
+            button.style.color = "#800000";
+            console.log(document.getElementsByClassName("sh-h-input__search-form")[0].nodeValue);
+            var br = document.createElement("br");
+            
+
+
+
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var raw = JSON.stringify({
+            "query": "gillette razor"
+            });
+
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+            };
+
+            
+
+            // fetch("https://81b49e9ba759.ngrok.io/getInfo", requestOptions)
+            // .then(response => response.text())
+            // .then(result => console.log(result))
+            // .catch(error => console.log('error', error));
+            document.getElementsByClassName("aULzUe IuHnof")[i].appendChild(br);
+            document.getElementsByClassName("aULzUe IuHnof")[i].appendChild(button);
+            }
+
+            // var scoreBtn = document.getElementById('aULzUe IuHnof');
+            // scoreBtn.addEventListener('click', function() {
+            //     console.log(document.getElementById("aULzUe IuHnof").textContent);
+            // });
+
+
+     
+            var br = document.createElement("br");
+            var div = document.createElement("div");
+            div.innerHTML = "Equify Recommends";
+            div.id = "altProducts";
+            div.type = "div";
+            div.style.backgroundColor = "pink";
+            div.style.color = "#800000";
+            div.style.fontWeight = "bold";
+            div.style.padding = "5px";
+            div.style.borderRadius = "8px";
+            div.style.border = "none";
+            document.getElementsByClassName("R3EKEf RlD7sd OtMIBd")[0].appendChild(div);
+            
+            
+            var i = 0;
+            for (i=0; i<5; i++){
+                var br = document.createElement("br");
+                document.getElementById("altProducts").appendChild(br);
+                var product = document.createElement("a");
+                product.innerHTML = `${response.alternates[i].title} - $ ${response.alternates[i].price_raw}`;
+                product.id = `suggestedProduct${i}`;
+                product.type = "a";
+                product.href = `${response.alternates[i].link}`
+                product.style.fontWeight = "100";
+                product.style.color = "#800000";
+                product.style.border = "none";
+                document.getElementById("altProducts").appendChild(product);
+            }
+            
+    } else {
+        console.log("Lost, Google")
+    
+    }
+    
+
+}
